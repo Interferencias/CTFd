91cc6101b374d10d359f6e49412cbbeb89440c57
German Martinez <germaaan@gmail.com>
2018-03-19 01:41:07 +0100

    Modifica vista principal

--- CTFd/views.py
+++ CTFd/views.py
@@ -39,19 +39,17 @@ def setup():
             # Index page
 
             index = """<div class="row">
-    <div class="col-md-6 offset-md-3">
+    <div class="col-md-12">
         <img class="w-100 mx-auto d-block" style="max-width: 500px;padding: 50px;padding-top: 14vh;" src="themes/core/static/img/logo.png" />
-        <h3 class="text-center">
-            <p>A cool CTF platform from <a href="https://ctfd.io">ctfd.io</a></p>
-            <p>Follow us on social media:</p>
-            <a href="https://twitter.com/ctfdio"><i class="fab fa-twitter fa-2x" aria-hidden="true"></i></a>&nbsp;
-            <a href="https://facebook.com/ctfdio"><i class="fab fa-facebook fa-2x" aria-hidden="true"></i></a>&nbsp;
-            <a href="https://github.com/ctfd"><i class="fab fa-github fa-2x" aria-hidden="true"></i></a>
-        </h3>
         <br>
-        <h4 class="text-center">
-            <a href="admin">Click here</a> to login and setup your CTF
-        </h4>
+        <h3 class="text-center">
+            <div style='font-size:0;'>
+                <div style='width:100%; margin:0 auto 0 auto; text-align:center; display:inline-block;'>
+                    <a href='https://interferencias.tech/'><img src='themes/core/static/img/interferencias.png' height="200px" alt='Logo Interferencias'></a>
+                    <a href='http://www.hackingdesdecero.org/'><img src='themes/core/static/img/hdc.png' height="190px" alt='Logo HDC'></a>
+                </div>
+            </div>
+        </h3>
     </div>
 </div>""".format(request.script_root)
 
